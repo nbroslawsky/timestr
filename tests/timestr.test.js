1099c0746767a9997964a76edb8dd496b20e6367
Matt Dunlap <matt.dunlap@welltok.com>
2015-04-16 09:49:39 -0600

    Disambiguation of test titles
    Several tests had identical names

--- tests/timestr.test.js
+++ tests/timestr.test.js
@@ -1,7 +1,7 @@
 var assert = require('chai').assert,
 	timestr = require('../index.js');
 
-describe('converting strings', function() {
+describe('days', function() {
 
 	var fifteenDaysShort = '15d',
 		fourteenDaysShort = '14d',
@@ -10,32 +10,38 @@ describe('converting strings', function() {
 		fifteenDaysLong = '15 days',
 		fourteenDaysLong = '14 days';
 
-	it('should convert to milliseconds properly', function() { assert.equal(timestr(fifteenDaysShort).toMilliseconds(),1296000000); });
-	it('should convert to seconds properly', function() { assert.equal(timestr(fifteenDaysShort).toSeconds(),1296000); });
-	it('should convert to minutes properly', function() { assert.equal(timestr(fifteenDaysShort).toMinutes(),21600); });
-	it('should convert to hours properly', function() { assert.equal(timestr(fifteenDaysShort).toHours(),360); });
-	it('should convert to days properly', function() { assert.equal(timestr(fifteenDaysShort).toDays(),15); });
-	it('should convert to weeks properly', function() { assert.equal(timestr(fourteenDaysShort).toWeeks(),2); });
+	describe('short string', function() {
+		it('should convert to milliseconds properly', function() { assert.equal(timestr(fifteenDaysShort).toMilliseconds(),1296000000); });
+		it('should convert to seconds properly', function() { assert.equal(timestr(fifteenDaysShort).toSeconds(),1296000); });
+		it('should convert to minutes properly', function() { assert.equal(timestr(fifteenDaysShort).toMinutes(),21600); });
+		it('should convert to hours properly', function() { assert.equal(timestr(fifteenDaysShort).toHours(),360); });
+		it('should convert to days properly', function() { assert.equal(timestr(fifteenDaysShort).toDays(),15); });
+		it('should convert to weeks properly', function() { assert.equal(timestr(fourteenDaysShort).toWeeks(),2); });
+	});
 
-	it('should convert to milliseconds properly', function() { assert.equal(timestr(fifteenDaysMedium).toMilliseconds(),1296000000); });
-	it('should convert to seconds properly', function() { assert.equal(timestr(fifteenDaysMedium).toSeconds(),1296000); });
-	it('should convert to minutes properly', function() { assert.equal(timestr(fifteenDaysMedium).toMinutes(),21600); });
-	it('should convert to hours properly', function() { assert.equal(timestr(fifteenDaysMedium).toHours(),360); });
-	it('should convert to days properly', function() { assert.equal(timestr(fifteenDaysMedium).toDays(),15); });
-	it('should convert to weeks properly', function() { assert.equal(timestr(fourteenDaysMedium).toWeeks(),2); });
+	describe('medium string', function() {
+		it('should convert to milliseconds properly', function() { assert.equal(timestr(fifteenDaysMedium).toMilliseconds(),1296000000); });
+		it('should convert to seconds properly', function() { assert.equal(timestr(fifteenDaysMedium).toSeconds(),1296000); });
+		it('should convert to minutes properly', function() { assert.equal(timestr(fifteenDaysMedium).toMinutes(),21600); });
+		it('should convert to hours properly', function() { assert.equal(timestr(fifteenDaysMedium).toHours(),360); });
+		it('should convert to days properly', function() { assert.equal(timestr(fifteenDaysMedium).toDays(),15); });
+		it('should convert to weeks properly', function() { assert.equal(timestr(fourteenDaysMedium).toWeeks(),2); });
+	});
 
-	it('should convert to milliseconds properly', function() { assert.equal(timestr(fifteenDaysLong).toMilliseconds(),1296000000); });
-	it('should convert to seconds properly', function() { assert.equal(timestr(fifteenDaysLong).toSeconds(),1296000); });
-	it('should convert to minutes properly', function() { assert.equal(timestr(fifteenDaysLong).toMinutes(),21600); });
-	it('should convert to hours properly', function() { assert.equal(timestr(fifteenDaysLong).toHours(),360); });
-	it('should convert to days properly', function() { assert.equal(timestr(fifteenDaysLong).toDays(),15); });
-	it('should convert to weeks properly', function() { assert.equal(timestr(fourteenDaysLong).toWeeks(),2); });
+	describe('long string', function() {
+		it('should convert to milliseconds properly', function() { assert.equal(timestr(fifteenDaysLong).toMilliseconds(),1296000000); });
+		it('should convert to seconds properly', function() { assert.equal(timestr(fifteenDaysLong).toSeconds(),1296000); });
+		it('should convert to minutes properly', function() { assert.equal(timestr(fifteenDaysLong).toMinutes(),21600); });
+		it('should convert to hours properly', function() { assert.equal(timestr(fifteenDaysLong).toHours(),360); });
+		it('should convert to days properly', function() { assert.equal(timestr(fifteenDaysLong).toDays(),15); });
+		it('should convert to weeks properly', function() { assert.equal(timestr(fourteenDaysLong).toWeeks(),2); });
+	});
 
 	it('should return false on bad inputs', function() { assert.equal(timestr('whatup').toSeconds(),false); });
 
 });
 
-describe('milliseconds', function() {
+describe('milliseconds strings', function() {
 
 	it('should convert 1 millisecond', function() {
 		assert.equal(timestr('1 ms').toMilliseconds(), 1);
@@ -54,7 +60,7 @@ describe('milliseconds', function() {
 });
 
 
-describe('seconds', function() {
+describe('seconds strings', function() {
 
 	it('should convert 1 second', function() {
 		assert.equal(timestr('1 s').toSeconds(), 1);
